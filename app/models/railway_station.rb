9a8ae7f1a65d1a03686320c432379f243ecec1a8
aizhan-ayapbergenova <aizhan@robarin.pro>
2021-09-16 14:26:46 +0600

    unmodified changes

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -4,4 +4,20 @@ class RailwayStation < ApplicationRecord
   has_many :routes, through: :railway_stations_routes
 
   validates :title, presence: true
+
+  scope :ordered, -> { joins(:railway_stations_routes).order("railway_stations_routes.position").uniq }
+
+  def update_position(route, position)
+    station_route = station_route(route)
+    station_route.update(position: position) if station_route
+  end
+
+  def position_in(route)
+    station_route(route).try(:position)
+  end
+
+  protected
+  def station_route(route)
+    @station_route ||= railway_stations_routes.where(route: route).first
+  end
 end
