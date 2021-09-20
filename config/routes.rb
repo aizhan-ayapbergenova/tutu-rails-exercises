a1d37f0d4027446ae902763e717b5d65baeb7833
aizhan-ayapbergenova <aizhan@robarin.pro>
2021-09-20 09:52:51 +0600

    add devise gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   resources :railway_stations do
     patch :update_position, on: :member
   end
@@ -11,6 +15,6 @@ Rails.application.routes.draw do
   
   get 'welcome/index'
 
-  root "welcome#index"
+  root to: "welcome#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
